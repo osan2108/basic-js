154a19cf2331437485a67da7c88223e65357b85c
osan2108 <96270126+osan2108@users.noreply.github.com>
2022-05-01 20:06:46 +0300

    Update hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -18,7 +18,7 @@ function calculateHanoi(/* disksNumber, turnsSpeed */) {
   //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
   let turns = 2 ** disksNumber - 1;
-  let seconds = turns / (turnsSpeed / 60 / 60);
+  let seconds = Math.floor(turns / (turnsSpeed / 60 / 60));
   return {
     turns: turns, seconds: seconds
   }
