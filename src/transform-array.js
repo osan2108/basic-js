3b060fddf2435372a62600d94e3ec99e917b0ba3
osan2108 <96270126+osan2108@users.noreply.github.com>
2022-05-01 20:18:46 +0300

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  //throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
   let transformedArr = [];
 
   if (!Array.isArray(arr)) {
@@ -23,18 +21,30 @@ function transform(/* arr */) {
   }
 
   for (let i = 0; i < arr.length; i++) {
+
     if (arr[i] === '--discard-prev') {
+
       arr[i - 1] && transformedArr.pop();
+
     } else if (arr[i] === '--discard-next') {
-        arr[i + 1] && i++ && transformedArr.push('del');
+
+      arr[i + 1] && i++ && transformedArr.push('del');
+
     } else if (arr[i] === '--double-prev') {
-        arr[i - 1] && transformedArr.push(transformedArr[transformedArr.length - 1]);
+
+      arr[i - 1] && transformedArr.push(transformedArr[transformedArr.length - 1]);
+
     } else if (arr[i] === '--double-next') {
-        arr[i + 1] && transformedArr.push(arr[i+1]);
+
+      arr[i + 1] && transformedArr.push(arr[i+1]);
+
     } else {
-        transformedArr.push(arr[i]);
+
+      transformedArr.push(arr[i]);
+      
     }
   }
+
   return transformedArr.filter( (item) => item !== 'del');
 }
 
