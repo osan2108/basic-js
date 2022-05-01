77067daae093c459813f43b39b60c7e634784ce7
osan2108 <96270126+osan2108@users.noreply.github.com>
2022-05-01 15:48:59 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -24,11 +24,7 @@ function createDreamTeam(members) {
     }
   }
 
-  dreamTeam.sort( (a, b) => {
-      if (a < b) return -1;
-      if (a > b) return 1;
-      return 0;
-  });
+  dreamTeam.sort()
 
   return dreamTeam.join('');
 }
