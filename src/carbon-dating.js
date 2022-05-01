b949c270716f8f776a09ffb81166d500803a180c
osan2108 <96270126+osan2108@users.noreply.github.com>
2022-05-01 14:00:20 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -20,7 +20,7 @@ const HALF_LIFE_PERIOD = 5730;
 function dateSample(/* sampleActivity */) {
   //throw new NotImplementedError('Not implemented');
   const k = 0.693;
-  if (isNaN(sampleActivity) || typeof sampleActivity == 'number' || sampleActivity <= 0){
+  if (isNaN(sampleActivity) || typeof sampleActivity != 'string' || sampleActivity <= 0){
       return false;
   } else {
   let time = (Math.log(MODERN_ACTIVITY / sampleActivity)) /(k / HALF_LIFE_PERIOD); 
